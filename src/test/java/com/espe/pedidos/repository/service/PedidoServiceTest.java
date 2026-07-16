c7f14b15f3e0b2971a40209bdaeb357573b7f7cc
hmquintero2 <hmqp2350537581@gmail.com>
2026-07-16 14:18:14 -0500

    red: test para crear pedido

---
+++ src/test/java/com/espe/pedidos/repository/service/PedidoServiceTest.java
@@ -0,0 +1,38 @@
+package com.espe.pedidos.repository.service;
+
+import com.espe.pedidos.repository.model.Pedido;
+import com.espe.pedidos.repository.repository.PedidoRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class PedidoServiceTest {
+
+    @Mock
+    private PedidoRepository pedidoRepository;
+
+    @InjectMocks
+    private PedidoService pedidoService;
+
+    @Test
+    void should_save_new_pedido_with_valid_data() {
+        // Arrange
+        Pedido nuevo = new Pedido("Luis", 40.0, "PENDIENTE");
+        given(pedidoRepository.save(any(Pedido.class))).willReturn(nuevo);
+
+        // Act
+        Pedido resultado = pedidoService.crearPedido(nuevo);
+
+        // Assert
+        assertThat(resultado.getCliente()).isEqualTo("Luis");
+        verify(pedidoRepository).save(nuevo);
+    }
+}
