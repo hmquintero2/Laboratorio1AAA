102f263487c21d65387585f1206f2db36984343c
hmquintero2 <hmqp2350537581@gmail.com>
2026-07-20 14:07:34 -0500

    green: implementa endpoint POST crear pedido

--- src/main/java/com/espe/pedidos/controller/PedidoController.java
+++ src/main/java/com/espe/pedidos/controller/PedidoController.java
@@ -2,10 +2,9 @@ package com.espe.pedidos.controller;
 
 import com.espe.pedidos.model.Pedido;
 import com.espe.pedidos.service.PedidoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +21,11 @@ public class PedidoController {
     @GetMapping
     public List<Pedido> porEstado(@RequestParam String estado) {
         return pedidoService.obtenerPorEstado(estado);
+    }
+
+    @PostMapping
+    public ResponseEntity<Pedido> crear(@RequestBody Pedido pedido) {
+        Pedido creado = pedidoService.crearPedido(pedido);
+        return ResponseEntity.status(HttpStatus.CREATED).body(creado);
     }
 }
